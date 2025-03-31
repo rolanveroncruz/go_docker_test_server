c543d77f2b21383b1a21b07c644aeedb44f6713a
rolanveroncruz <rolanveroncruz@gmail.com>
2025-03-31 15:55:55 +0800

    first commit: ping

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"net/http"
+	"os"
+)
+
+type StringResponse struct {
+	Message string `json:"Message"`
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+	portStr := ":" + os.Getenv("PORT")
+
+	mux_router := mux.NewRouter()
+	mux_router.HandleFunc("/ping", ping)
+	println("Listening on port " + portStr + "...")
+	err = http.ListenAndServe(portStr, mux_router)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	response := StringResponse{
+		Message: "Hello, World!",
+	}
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		panic(err)
+	}
+	_, err = w.Write(jsonResponse)
+	if err != nil {
+		panic(err)
+	}
+}
